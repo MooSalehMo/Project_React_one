e23df347b64d30771fa224b18c84b32507aedd01
Mohamed Saleh <mo01144368997>
2020-10-04 19:39:39 +0200

    update in template

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-                <BrowserRouter>
-			        <Navbar />
-			        <Route exact path="/" component={HomePage} />
-                    <Route path="/Contact" component={ContactPage} />
-					<Footer />
+              <BrowserRouter>
+			            <Navbar />
+			            <Route exact path="/" component={HomePage} />
+                  <Route path="/Contact" component={ContactPage} />
+				   	<Footer />
 				</BrowserRouter>
 			</div>
 		);
@@ -25,7 +25,7 @@ class App extends Component {
 export default App;
 
 
-/* 
+/*
 
   1 - npm install node-sass axios styled react-router-dom --save
   2- npm add bootstrap react-bootstrap
